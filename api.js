8c6ac448c524b43925ecfa56f4e496c5f2e9eae9
Jison-James <150419153+Jison-James@users.noreply.github.com>
2025-08-09 01:12:03 +0530

    Update api.js

--- api.js
+++ api.js
@@ -155,9 +155,9 @@ async function handleFaceRoast(file) {
 
     // Send expression to Groq for roasting
     const insult = await groqPrompt(
-       `1: "Your emotion is: <emotion>" — keep it short and clear.
+      `1: "Your emotion is: <emotion>" — keep it short and clear.
       2: Roast them in a mocking, NSFW, funny style.
-       Be chaotic, sarcastic, and add emojis fitting the roast.(mock in three to four sentence)
+       Be chaotic, sarcastic, and add emojis fitting the roast.
    Make sure the first line is ONLY the emotion statement and
    the second line is ONLY the roast.`
     );
@@ -178,3 +178,4 @@ export {
   handleGeneralChat,
   handleFaceRoast
 };
+
